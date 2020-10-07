d7cabb4f3d4760b9415cb5d472ead4557cbd44c9
Ubsefor <letzdropthebas@gmail.com>
2020-10-07 17:05:45 +0300

    Initial Commit

---
+++ mortal-stack/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  mortal-stack
+//
+//  Created by Александр Махов on 7/10/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
